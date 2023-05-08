6da83ae98e0b6994063cc7c0740c4f85d1644258
Ryan Maguire <45566414+Magzlar@users.noreply.github.com>
2023-05-08 18:44:08 +0100

    Delete Random_PasswordGen.py

--- Random_PasswordGen.py
+++
@@ -1,9 +0,0 @@
-import random 
-
-ter = "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ_+=[}{"
-
-password = ""
-for t in range(15): 
-    password += random.choice(ter)
-
-print(password)

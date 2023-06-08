20ed6710deb4a256c3b05c5441ff39b3e8538c4e
Ryan Maguire <45566414+Magzlar@users.noreply.github.com>
2023-06-08 18:51:46 +0100

    Update RadCalculate_Amount_Left.py

--- RadCalculate_Amount_Left.py
+++ RadCalculate_Amount_Left.py
@@ -1,32 +1,38 @@
-# Calculate the amount of radioactivity left in a sample following decay overtime
-# Require three variables; Time Passed, Current activity of radioisotope and Half Life of the Radioisotope. 
-
 import math
+import Levenshtein
 
-e = 0.5
-nt = float(input("Enter current amount of radioactivity (CPM): "))
-t = float(input("Enter amount of time passed (Days): "))
+class Radioactive:
+    e = math.log(2)  # Euler's number remains constant
 
-choice1 = input("Are you using 1) tritium or 2) Iodine 125 :")
-choice1 = float(choice1)
-    
-if choice1 == 1 or "one" or "1":
-        T = 365*12.5
+    def __init__(self, nt: float, t: float):
+        """Class receives three parameters:
+        nt = The amount of radioactivity in the sample when received,
+        t = The time passed since the sample was received,
+        T = The type of isotope - Tritium or Iodine"""
+        self.nt = nt
+        self.t = t
+        choice = input("Are you using 1) Tritium? or 2) Iodine 125? :")
+        if self.is_approximate_match(choice, ["1", "tritium"]):
+            self.T = 365 * 12.5
+        elif self.is_approximate_match(choice, ["2", "iodine"]):
+            self.T = 59.86
+        else:
+            raise ValueError("Invalid choice.")
 
-elif choice1 == 2:
-        T = 59.86
-        
-elif choice1 != 1 or 2:
-        print("Thats not an option")
+    @staticmethod
+    def is_approximate_match(word, expected_words, threshold=2):
+        for expected_word in expected_words:
+            distance = Levenshtein.distance(word.lower(), expected_word.lower())
+            if distance <= threshold:
+                return True
+        return False
 
-def func(nt,e,t, T):
-    
-    return nt*e**(t/T)
+    def decay(self):
+        return self.nt * Radioactive.e ** (self.t / self.T)
 
-result = func(nt,e,t,T)
+    def __str__(self):
+        return f"The CPM of your sample is now {self.decay()}"
 
-result1 = int(result)
-
-print(result1, "CPM remain")
-
-  
+if __name__ == '__main__':
+    answer1 = Radioactive(1500, 15)
+    print(answer1.decay())
